a2d6d17b28f98b2696ad9f47037af3c1cd6a12b1
Danang Bagus <89261615+bgsdanang@users.noreply.github.com>
2021-08-21 13:11:54 +0700

    Add files via upload

---
+++ Belajar/R/Introduction to Data Science with R.R
@@ -0,0 +1,28 @@
+# Hello World
+
+"Hello World"
+
+# Praktek Angka dan Perhitungan
+
+10 + 7
+
+# Variable dalam R
+
+a = 5
+print(a)
+
+# Praktik Penggunaan Comment
+
+10+7 #Ini adalah baris komentar	
+
+# Praktek Penggunaan Vector - Bagian Satu
+
+c(3, 10, 15)
+
+# Praktek Penggunaan Vector - Bagian Dua
+
+c(1:5)
+
+# Menggunakan Fungsi Summary
+
+summary(c(1:5))
